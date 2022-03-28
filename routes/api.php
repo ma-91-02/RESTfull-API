855800d7bbadfb59e7aace200e4e3de0cd71a936
ma-91-02 <ma.91.02@mail.ru>
2022-03-28 09:20:03 +0300

    update

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Lesson;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,27 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('lessons',function(){
+    return Lesson::all();
+});
+
+Route::get('lessons/{id}',function($id){
+    return Lesson::find($id);
+});
+
+Route::post('lessons',function(Request $request){
+    return Lesson::create($request->all());
+});
+
+Route::put('lessons/{id}',function(Request $request, $id){
+    $lesson = Lesson::findOrFail($id);
+    $lesson->update($request->all());
+    return $lesson;
+});
+
+Route::delete('lessons/{id}',function($id){
+    Lesson::find($id)->delete();
+    return 204;
 });
